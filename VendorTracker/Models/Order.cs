3db6d8b3d00fd673c2e3b8e9d7f02ec6777758e5
Michael Carroll <128497239+mcarroll138@users.noreply.github.com>
2023-07-21 12:33:31 -0700

    good pass OrderConstructor_CreateInstanceOfOrder.

--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -4,6 +4,19 @@ namespace VendorTracker.Models
 {
   public class Order
   {
+    public string Description { get; set; }
+    private static List<Order> _instances = new List<Order> { };
 
+    public Order(string description)
+    {
+      Description = description;
+      _instances.Add(this);
+      // ID = _instances.Count;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
   }
 }
