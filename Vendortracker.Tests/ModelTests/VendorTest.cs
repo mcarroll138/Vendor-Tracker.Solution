ec6634cff23227d996bd144306a394c7e7addceb
Michael Carroll <128497239+mcarroll138@users.noreply.github.com>
2023-07-21 10:34:22 -0700

    good fail VendorTest, VendorConstuctor Test.

---
+++ Vendortracker.Tests/ModelTests/VendorTest.cs
@@ -0,0 +1,37 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace VendorTracker.Tests
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor()
+    {
+      Vendor newVendor = new Vendor("test vendor");
+      AssemblyLoadEventArgs.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      //Arrange
+      string name = "test vendor";
+      Vendor newVendor = new Vendor(name);
+
+      //Act
+      string result = newVendor.Name;
+
+      //Assert
+      Assert.AreEqual(name, result);
+    }
+  }
+}
