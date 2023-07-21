8ecbeaa7874900b3bd4482268927e17349d9b83f
Michael Carroll <128497239+mcarroll138@users.noreply.github.com>
2023-07-21 11:59:50 -0700

    good fail OrderConstructor_CreateInstanceOfOrder.

---
+++ Vendortracker.Tests/ModelTests/OrderTest.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using VendorTracker.Models;
+using System;
+
+namespace VendorTracker.Tests
+{
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder()
+    {
+      Order newOrder = new Order("bread");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  }
+}
