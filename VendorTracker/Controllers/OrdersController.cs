731e8e48d688475c7de8d65934cdf749e25646a5
Michael Carroll <128497239+mcarroll138@users.noreply.github.com>
2023-07-21 21:42:02 -0700

    update Orders Controller, debug orders controllers

--- VendorTracker/Controllers/OrdersController.cs
+++ VendorTracker/Controllers/OrdersController.cs
@@ -5,5 +5,24 @@ using System.Collections.Generic;
 namespace VendorTracker.Controllers
 {
 
+  public class OrdersController : Controller
+  {
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
 
+    [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Show(int vendorsId, int orderId)
+    {
+      Order order = Order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorsId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("order", order);
+      model.Add("vendor", vendor);
+      return View(model);
+    }
+  }
 }
