2edf1e65fa6bb0d0fedc9501594bb1ebcf9fc3ca
Michael Carroll <128497239+mcarroll138@users.noreply.github.com>
2023-07-23 13:47:00 -0700

    update README, debug Orders/New.cshtml

--- VendorTracker/Controllers/VendorsController.cs
+++ VendorTracker/Controllers/VendorsController.cs
@@ -39,7 +39,20 @@ namespace VendorTracker.Controllers
       return View(model);
     }
 
-    [HttpPost("/vendors/{vendorId}/orders")]
+    // [HttpPost("/vendors/{vendorId}/orders")]
+    // public ActionResult Create(int vendorId, string orderDescription)
+    // {
+    //   Dictionary<string, object> model = new Dictionary<string, object>();
+    //   Vendor foundVendor = Vendor.Find(vendorId);
+    //   Order newOrder = new Order(orderDescription);
+    //   foundVendor.AddOrder(newOrder);
+    //   List<Order> vendorOrders = foundVendor.Orders;
+    //   model.Add("orders", vendorOrders);
+    //   model.Add("vendor", foundVendor);
+    //   return View("Show", model);
+    // }
+
+    [HttpPost("/vendors/{vendoryId}/orders")]
     public ActionResult Create(int vendorId, string orderDescription)
     {
       Dictionary<string, object> model = new Dictionary<string, object>();
